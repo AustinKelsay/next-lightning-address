13cf8d8f730a6f13538e78c0ec9086e7d977a4f5
austinkelsay <austinkelsay@yahoo.com>
2023-12-22 09:53:49 -0600

    Fix import

--- pages/api/getLnurl.js
+++ pages/api/getLnurl.js
@@ -1,5 +1,5 @@
 import { bech32 } from 'bech32';
-import { runMiddleware, corsMiddleware } from "../../utils/apiMiddleware";
+import { runMiddleware, corsMiddleware } from "../../utils/middleware";
 
 function encodeLnurl(url) {
     const words = bech32.toWords(Buffer.from(url, 'utf8'));
@@ -8,7 +8,7 @@ function encodeLnurl(url) {
 
 export default function handler(req, res) {
     runMiddleware(req, res, corsMiddleware);
-    
+
     const originalUrl = `${process.env.BACKEND_URL}/api/lnurl`
     const encodedLnurl = encodeLnurl(originalUrl);
 
