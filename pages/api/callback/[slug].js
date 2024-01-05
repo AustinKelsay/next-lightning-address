77d19db00029db653d873cab3a319613cc910f44
austinkelsay <austinkelsay@yahoo.com>
2024-01-05 09:17:24 -0600

    Add name to callback

---
+++ pages/api/callback/[slug].js
@@ -0,0 +1,47 @@
+import axios from "axios"
+import crypto from "crypto"
+import { runMiddleware, corsMiddleware } from "@/utils/middleware";
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export default async function handler(req, res) {
+    await runMiddleware(req, res, corsMiddleware);
+
+    const { slug, ...queryParams } = req.query
+
+    if (slug === 'bitcoinplebdev') {
+        if (queryParams.amount) {
+            const metadata = [
+                ["text/plain", "Sample LNURL-PAY endpoint"]
+            ];
+
+            const metadataString = JSON.stringify(metadata);
+
+            const hash = crypto.createHash('sha256').update(metadataString).digest('hex');
+
+            const descriptionHash = Buffer.from(hash, 'hex').toString('base64'); // Encoding as base64
+
+            // Convert amount from millisatoshis to satoshis
+            const value = parseInt(queryParams.amount) / 1000;
+
+            if (value < 1) {
+                res.status(400).json({ error: 'Amount too low' })
+                return
+            } else if (value > 1000) {
+                res.status(400).json({ error: 'Amount too high' })
+                return
+            } else {
+                axios.post(`${BACKEND_URL}/api/lnd`, { amount: value, descriptionHash: descriptionHash })
+                    .then(function (response) {
+                        res.status(200).json({ pr: response.data })
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        } else {
+            res.status(400).json({ error: 'Amount not specified' })
+            return
+        }
+    }
+}
