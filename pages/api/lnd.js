d62dfc2d695a6f419acd92d28946c163ce07ae1e
austinkelsay <austinkelsay@yahoo.com>
2024-01-06 11:52:45 -0600

    comments, remove ssl locally

--- pages/api/lnd.js
+++ pages/api/lnd.js
@@ -1,23 +1,31 @@
 import axios from "axios"
+import https from 'https';
+
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
 
 const LND_HOST = process.env.LND_HOST;
 const LND_MACAROON = process.env.LND_MACAROON;
 
 export default async function handler(req, res) {
-    try {
-        const response = await axios.post(`${LND_HOST}/v1/invoices`, {
-            value: req.body.amount,
-            description_hash: req.body.description_hash
-        }, {
-            headers: {
-                'Grpc-Metadata-macaroon': LND_MACAROON,
-            }
-        });
+   try {
+       const response = await axios.post(`${LND_HOST}/v1/invoices`, {
+           value: req.body.amount,
+           description_hash: req.body.description_hash
+       }, {
+           headers: {
+               'Grpc-Metadata-macaroon': LND_MACAROON,
+           },
+          // this is to disable SSL verification for the LND cert locally
+          // be sure to remove this in production   
+          // httpsAgent: agent
+       });
 
-        res.status(200).json(response.data.payment_request);
-    } catch (error) {
-        // console.error(inspect(error, false, undefined, true));
-        console.error('Error (server) fetching data from LND:', error.message);
-        res.status(500).json({ message: 'Error fetching data' });
-    }
+       res.status(200).json(response.data.payment_request);
+   } catch (error) {
+       // console.error(inspect(error, false, undefined, true));
+       console.error('Error (server) fetching data from LND:', error.message);
+       res.status(500).json({ message: 'Error fetching data' });
+   }
 }
--- pages/api/lnurl.js
+++ pages/api/lnurl.js
@@ -7,6 +7,8 @@ export default async function handler(req, res) {
         ["text/plain", "Sample LN-ADDRESS endpoint"]
     ];
     const response = {
+        // callback refers to the endpoint that will be called by the LNURL server to get the invoice
+        // This callback endpoint will be the same one we use for our lightning address
         callback: `${process.env.BACKEND_URL}/api/callback/bitcoinplebdev`,
         maxSendable: 100000000, // milisatoshis
         minSendable: 1000,      // milisatoshis
